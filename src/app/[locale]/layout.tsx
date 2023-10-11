0218a99f128f5bb6fc4cf279ee2e34635d05ec81
Timi-Duban <ti.mi.34@hotmail.fr>
2023-10-11 11:40:27 +0300

    translation provider (for client) and SSG

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
+import initTranslations from '@/i18n';
+import TranslationsProvider from '@/components/translationsProvider';
+import i18nConfig from '../../../i18nConfig';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,19 +13,27 @@ export const metadata: Metadata = {
   description: 'A page about this Freelance developper',
 }
 
+export function generateStaticParams() {
+  return i18nConfig.locales.map(locale => ({ locale }));
+}
+
 interface RootProps {
   children: React.ReactNode
   params: {
     locale: string
   }
 }
+async function RootLayout({children, params}: RootProps) {
+  const { locale } = params;
+  const { t, options } = await initTranslations(locale, ['common']);
 
-function RootLayout({children, params}: RootProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar locale={params.locale}/>
-        {children}
+        <TranslationsProvider namespaces={options.ns} locale={locale}>
+          <Navbar locale={locale}/>
+          {children}
+        </TranslationsProvider>
         </body>
     </html>
   )
