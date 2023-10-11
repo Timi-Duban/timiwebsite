2ce877763bff4b5807842b23bd4ed199f9a5d1a8
Timi-Duban <ti.mi.34@hotmail.fr>
2023-10-11 11:41:07 +0300

    add contact form

---
+++ src/components/contact/form.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface IFormProps {locale: string}
+export default function Form (props: IFormProps) {
+    const { locale } = props;
+    const { t } = useTranslation();
+    const [res, setRes] = useState('');
+
+    const handleSubmit = (e: any) => {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        setRes('Please wait...');
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = JSON.stringify(Object.fromEntries(formData.entries()));
+
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: formJson
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                setRes(json.message);
+            } else {
+                console.log(response);
+                setRes(json.message);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            setRes('Something went wrong!');
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                setRes('');
+            }, 3000);
+        });
+    };
+    
+    return (
+        <div className='flex flex-1 justify-center my-8'>
+            <form
+                className='w-3/4' 
+                method='post' onSubmit={handleSubmit}
+            >
+                <input type='hidden' name='access_key' value='4df3931f-4d05-4e2f-ae1d-10a593c366a8' />
+                <div className='flex flex-wrap -mx-3 mb-4'>
+                    <div className='w-full md:w-1/2 px-3 mb-4 md:mb-0'>
+                        <label className='block uppercase tracking-wide md:text-neutral-50 text-xs font-bold'>
+                            {t('name')}
+                        <input 
+                            className='appearance-none focus:outline-none focus:bg-white focus:border-neutral-500 block w-full rounded py-3 px-4 leading-tight bg-neutral-100 md:bg-neutral-200 text-neutral-700 border border-neutral-200'
+                            type='text' placeholder={t('name')} name='name' required
+                        />
+                        </label>
+                    </div>
+                    <div className='w-full md:w-1/2 px-3'>
+                        <label className='block uppercase tracking-wide md:text-neutral-50 text-xs font-bold'>
+                            Email
+                        <input 
+                            className='appearance-none focus:outline-none focus:bg-white focus:border-neutral-500 block w-full rounded py-3 px-4 leading-tight bg-neutral-100 md:bg-neutral-200 text-neutral-700 border border-neutral-200'
+                            type='email' placeholder='Email' name='email' required
+                        />
+                        </label>
+                    </div>
+                </div>
+                <div className='flex flex-wrap -mx-3 mb-4'>
+                    <div className='w-full px-3'>
+                        <label className='block uppercase tracking-wide md:text-neutral-50 text-xs font-bold'>
+                            {t('subject')}
+                        <input 
+                            className='appearance-none focus:outline-none focus:bg-white focus:border-neutral-500 block w-full rounded py-3 px-4 leading-tight bg-neutral-100 md:bg-neutral-200 text-neutral-700 border border-neutral-200'
+                            type='text' name='subject' placeholder={t('subjectPlaceholder')} required
+                        />
+                        </label>
+                    </div>
+                </div>
+                <div className='flex flex-wrap -mx-3 mb-4'>
+                    <div className='w-full px-3'>
+                        <label className='block uppercase tracking-wide md:text-neutral-50 text-xs font-bold'>
+                            Message
+                        <textarea
+                            className='no-resize appearance-none block w-full bg-neutral-100 md:bg-neutral-200 text-neutral-700 border border-neutral-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-neutral-500 h-48 resize-none'
+                            name='message' placeholder='Message'
+                        />
+                        </label>
+                    </div>
+                </div>
+                <button className='w-full shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'>
+                Send
+                </button>
+
+                <p className='md:text-neutral-50 text-lg font-bold text-center mt-2 '>{res}</p>
+            </form>
+        </div>
+    );
+}
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,6 +1,7 @@
 import initTranslations from "@/i18n";
 import Title from "../title";
 import { DownloadIcon } from "../icons";
+import Form from "./form";
 
 interface IContactProps {locale: string}
 export default async function Contact (props: IContactProps) {
@@ -8,9 +9,13 @@ export default async function Contact (props: IContactProps) {
   const { t } = await initTranslations(locale, ['common']);
 
   return (
-    <div className='bg-neutral-200 md:bg-neutral-700 p-2 pt-0'>
+    <div className='bg-neutral-200 md:bg-neutral-700 pb-2 pt-0'>
       <Title title={t('contact')} />
-      <div className='flex justify-center md:text-neutral-50 text-center'>
+      <div className='flex'>
+        <Form locale={locale} />
+        <div className='bg-blue-300 flex-1 opacity-30' />
+      </div>
+      <div className='flex justify-center md:text-neutral-50 text-center pt-6'>
         <DownloadIcon />
         <p className='ml-1'>{t('downloadCV')}</p>
       </div>
